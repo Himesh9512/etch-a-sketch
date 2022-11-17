9ed981f69acac193ccb2ae6d19d255477e1d1d0c
Himesh9512 <himeshjangid0000@gmail.com>
2022-11-17 14:50:02 +0530

    Add changing mode method

--- js/script.js
+++ js/script.js
@@ -1,18 +1,29 @@
 const DEFAULT_COLOR = '#262626';
 const DEFAULT_SIZE = 16;
+const DEFAULT_MODE = 'colorMode';
 
 let color = DEFAULT_COLOR;
 let size = DEFAULT_SIZE;
+let mode = DEFAULT_MODE;
 
 const container = document.getElementById('grid');
 const userColor = document.getElementById('chooseColor');
+const userMode = document.getElementById('selectMode');
+
+userMode.addEventListener('change', () => {
+    setMode(userMode.value);
+})
 
 userColor.addEventListener('change', () => {
     setColor(userColor.value);
 })
 
-function setColor(userColor){
+function setColor(userColor) {
     color = userColor;
+}
+
+function setMode(userMode){
+    mode = userMode;
 }
 
 function setGrid() {
@@ -28,7 +39,15 @@ function setGrid() {
 }
 
 const changeColor = (e) => {
-    e.target.style.backgroundColor = color;
+    if (mode === 'rainbowMode') {
+        let red = Math.floor(Math.random() * 256);
+        let green = Math.floor(Math.random() * 256);
+        let blue = Math.floor(Math.random() * 256);
+
+        e.target.style.backgroundColor = `rgb(${red},${green},${blue})`;
+    } else {
+        e.target.style.backgroundColor = color;
+    }
 }
 
 window.onload = () => {
